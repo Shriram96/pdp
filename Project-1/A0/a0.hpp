76555999925e192c0a0c97fc7169f93a93b60c83
Shriram Ravi <shriramr@srv-p22-13.cbls.ccr.buffalo.edu>
2021-10-05 20:15:25 -0400

    Test 1

--- Project-1/A0/a0.hpp
+++ Project-1/A0/a0.hpp
@@ -5,7 +5,7 @@
 
 #ifndef A0_HPP
 #define A0_HPP
-
+#include <omp.h>
 #include <vector>
 
 void get_sub_matrix1(int n, int m, int start_n, int start_m, const std::vector<float>& K, std::vector<float>& A)
@@ -79,22 +79,54 @@ void filter_2d(int n, int m, const std::vector<float>& K, std::vector<float>& A)
     // int total_matrices = 0;
     unsigned int i = 0;
     unsigned int j = 0;
+    unsigned int k = 0;
+    int sum = 0;
     std::vector<float> A1(n * m);
-    #pragma omp parallel for shared(K,A,A1) private(i,j) collapse(2)
-    // {
-        // #pragma omp for schedule(static)
+    std::cout << omp_get_max_threads() << std::endl;
+    #pragma omp parallel for shared(K,A,A1,std::cout) private(i,j,k,sum) collapse(2) schedule(dynamic)
         for(i = 0; i <= n-3; i++)
         {
-            // #pragma omp task shared(A, A1, K)
             for(j = 0; j <= m-3; j++)
             {
                 // std::cout << ++total_matrices << ")" << std::endl;
-                A1[(i * n) + j] = get_sub_matrix(n, m, i, j, K, A);
+                // A1[(i * n) + j] = get_sub_matrix(n, m, i, j, K, A);
                 // get_sub_matrix1(n, m, i, j, K, A);
                 // std::cout << std::endl << std::endl << std::endl;
+                sum += (A.at((i * n) + j) * K.at(0) \
+                    + A.at((i * n) + (j + 1)) * K.at(3) \
+                    + A.at((i * n) + (j + 2)) * K.at(6));
+                sum += (A.at((i * n) + j) * K.at(1) \
+                    + A.at((i * n) + (j + 1)) * K.at(4) \
+                    + A.at((i * n) + (j + 2)) * K.at(7));
+                sum += (A.at((i * n) + j) * K.at(2) \
+                    + A.at((i * n) + (j + 1)) * K.at(5) \
+                    + A.at((i * n) + (j + 2)) * K.at(8));
+
+                sum += (A.at(((i + 1) * n) + j) * K.at(0) \
+                    + A.at(((i + 1) * n) + (j + 1)) * K.at(3) \
+                    + A.at(((i + 1) * n) + (j + 2)) * K.at(6));
+                sum += (A.at(((i + 1) * n) + j) * K.at(1) \
+                    + A.at(((i + 1) * n) + (j + 1)) * K.at(4) \
+                    + A.at(((i + 1) * n) + (j + 2)) * K.at(7));
+                sum += (A.at(((i + 1) * n) + j) * K.at(2) \
+                    + A.at(((i + 1) * n) + (j + 1)) * K.at(5) \
+                    + A.at(((i + 1) * n) + (j + 2)) * K.at(8));
+
+                sum += (A.at(((i + 2) * n) + j) * K.at(0) \
+                    + A.at(((i + 2) * n) + (j + 1)) * K.at(3) \
+                    + A.at(((i + 2) * n) + (j + 2)) * K.at(6));
+                sum += (A.at(((i + 2) * n) + j) * K.at(1) \
+                    + A.at(((i + 2) * n) + (j + 1)) * K.at(4) \
+                    + A.at(((i + 2) * n) + (j + 2)) * K.at(7));
+                sum += (A.at(((i + 2) * n) + j) * K.at(2) \
+                    + A.at(((i + 2) * n) + (j + 1)) * K.at(5) \
+                    + A.at(((i + 2) * n) + (j + 2)) * K.at(8));
+                A1[((i + 1) * n) + (j + 1)] = sum;
+                // std::cout << omp_get_thread_num() << std::endl;
+                #pragma omp critical
+                std::cout << i+1 << 'x' << j+1 << " Thread:" << omp_get_thread_num() << std::endl;
             }
         }
-    // }
     A = A1;
 } // filter_2d
 
