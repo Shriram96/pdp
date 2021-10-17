a1a2a23452e089f69103ed4d1696f800d6f0523f
Shriram Ravi <shriramr@srv-p22-13.cbls.ccr.buffalo.edu>
2021-10-17 19:12:53 -0400

    Slurm change

--- Project-1/A0/slurm.sh
+++ Project-1/A0/slurm.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ####### PLANEX SPECIFIC - DO NOT EDIT THIS SECTION
-#SBATCH --clusters=ub-hpc
-#SBATCH --qos=general-compute
+#SBATCH --clusters=faculty
+#SBATCH --partition=planex
+#SBATCH --qos=planex
+#SBATCH --account=cse570f21
 #SBATCH --exclusive
 #SBATCH --mem=64000
 #SBATCH --output=%j.stdout
@@ -11,15 +13,15 @@
 ####### CUSTOMIZE THIS SECTION FOR YOUR JOB
 #SBATCH --job-name="changeme"
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=32
-#SBATCH --time=03:00:00
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=01:30:00
 
 module load gcc/10.2.0
 cd "/user/shriramr/pdp/Project-1/A0"
 make clean all
 threads=( 1 2 4 6 8 10 )
 volumes=( 1000 2000 4000 6000 8000 10000 )
-tests=1
+tests=3
 for i in "${volumes[@]}"
 do
 	for j in "${threads[@]}"
@@ -31,9 +33,6 @@ do
       for (( k = 0; k < "$tests"; k++ ))
       do
          OMP_NUM_THREADS="$j" ./a0 100000 "$i"
-      done
-      for (( k = 0; k < "$tests"; k++ ))
-      do
          OMP_NUM_THREADS="$j" ./a0 "$i" 100000
       done
       echo "END:############################################################"
