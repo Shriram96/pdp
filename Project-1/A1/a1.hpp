f286bc7e0df9109c3e0e9574196927ee4ad7ecf7
Shriram Ravi <shriramr@srv-p22-13.cbls.ccr.buffalo.edu>
2021-10-18 17:16:46 -0400

    A1 initial commit

---
+++ Project-1/A1/a1.hpp
@@ -0,0 +1,16 @@
+/*  YOUR_FIRST_NAME
+ *  YOUR_LAST_NAME
+ *  YOUR_UBIT_NAME
+ */
+
+#ifndef A1_HPP
+#define A1_HPP
+
+#include <vector>
+
+
+void isort(std::vector<short int>& Xi, MPI_Comm comm) {
+
+} // isort
+
+#endif // A1_HPP
