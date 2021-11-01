4464c42e31943e5e75d387704cb44752faee48b3
Shriram Ravi <shriramr@srv-p22-13.cbls.ccr.buffalo.edu>
2021-11-01 15:04:32 -0400

    With Scatter

--- Project-1/A1/a1.hpp
+++ Project-1/A1/a1.hpp
@@ -31,30 +31,33 @@ void isort(std::vector<short int>& Xi, MPI_Comm comm) {
     short int keyvalues[countersize + 1] = {0};
     keyvalues[countersize] = SHRT_MIN;
 
+    unsigned long long int counteri[2] = {0};
+    short int keyvaluesi[2] = {0};
+
     for (int i = 0; i < Xi.size(); i++)
         counter[Xi[i] + size - 1] = counter[Xi[i] + size - 1] + 1;
     
     MPI_Barrier(comm);
     MPI_Allreduce(&counter, &globalcounter, countersize, MPI_UNSIGNED_LONG_LONG, MPI_SUM, comm);
+    // MPI_Reduce(&counter, &globalcounter, countersize + 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0, comm);
     MPI_Barrier(comm);
-    // MPI_Reduce(&counter, &globalcounter, countersize, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0, comm);
 
     for(int i = 0; i < countersize; i++)
     {
         keyvalues[i] = i-size+1;
     }
 
-    // MPI_Barrier(comm);
-    // if (rank == 0)
-    // {
-    //     std::cout << "Counts:" << std::endl;
-    //     for(int i = 0; i < countersize; i++)
-    //     {
-    //         std::cout << keyvalues[i] << ":\t" << globalcounter[i] << std::endl;
-    //     }
-    //     std::cout << std::endl;
-    // }
-    // MPI_Barrier(comm);
+    MPI_Barrier(comm);
+    if (rank == 0)
+    {
+        std::cout << "Counts:" << std::endl;
+        for(int i = 0; i < countersize; i++)
+        {
+            std::cout << keyvalues[i] << ":\t" << globalcounter[i] << std::endl;
+        }
+        std::cout << std::endl;
+    }
+    MPI_Barrier(comm);
 
     for(int i = 0; i < countersize-1; i++)
     {
@@ -81,6 +84,11 @@ void isort(std::vector<short int>& Xi, MPI_Comm comm) {
             }
         }
     }
+
+    MPI_Barrier(comm);
+    MPI_Scatter(&globalcounter, 2, MPI_UNSIGNED_LONG_LONG, &counteri, 2, MPI_UNSIGNED_LONG_LONG, 0, comm);
+    MPI_Scatter(&keyvalues, 2, MPI_SHORT, &keyvaluesi, 2, MPI_SHORT, 0, comm);
+    MPI_Barrier(comm);
 
     unsigned long long int totalsize = 0;
     totalsize += globalcounter[rank];
@@ -116,6 +124,7 @@ void isort(std::vector<short int>& Xi, MPI_Comm comm) {
                 std::cout << keyvalues[i] << ":\t" << globalcounter[i] << std::endl;
             }
             std::cout << std::endl;
+            std::cout << "Key:Counteri" << std::endl << keyvaluesi[0] << ":" << counteri[0] << std::endl << keyvaluesi[1] << ":" << counteri[1] << std::endl;
         }
         // MPI_Barrier(comm);
         // if (rank == (size - 1))
