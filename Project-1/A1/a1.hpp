c075ddc199ade763ab27963f0c8500ecfad1eeb6
Shriram Ravi <shriramr@srv-p22-13.cbls.ccr.buffalo.edu>
2021-11-01 14:42:45 -0400

    Equal filling in logic

--- Project-1/A1/a1.hpp
+++ Project-1/A1/a1.hpp
@@ -7,6 +7,7 @@
 #define A1_HPP
 
 #include <vector>
+#include <climits>
 
 
 void isort(std::vector<short int>& Xi, MPI_Comm comm) {
@@ -25,8 +26,10 @@ void isort(std::vector<short int>& Xi, MPI_Comm comm) {
     short int countersize = 2*size-1;
     // std::vector<int> counter(countersize, 0);
     // std::vector<int> globalcounter(countersize, 0);
-    unsigned long long int counter[countersize] = {0};
-    unsigned long long int globalcounter[countersize] = {0};
+    unsigned long long int counter[countersize + 1] = {0};
+    unsigned long long int globalcounter[countersize + 1] = {0};
+    short int keyvalues[countersize + 1] = {0};
+    keyvalues[countersize] = SHRT_MIN;
 
     for (int i = 0; i < Xi.size(); i++)
         counter[Xi[i] + size - 1] = counter[Xi[i] + size - 1] + 1;
@@ -35,6 +38,49 @@ void isort(std::vector<short int>& Xi, MPI_Comm comm) {
     MPI_Allreduce(&counter, &globalcounter, countersize, MPI_UNSIGNED_LONG_LONG, MPI_SUM, comm);
     MPI_Barrier(comm);
     // MPI_Reduce(&counter, &globalcounter, countersize, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0, comm);
+
+    for(int i = 0; i < countersize; i++)
+    {
+        keyvalues[i] = i-size+1;
+    }
+
+    // MPI_Barrier(comm);
+    // if (rank == 0)
+    // {
+    //     std::cout << "Counts:" << std::endl;
+    //     for(int i = 0; i < countersize; i++)
+    //     {
+    //         std::cout << keyvalues[i] << ":\t" << globalcounter[i] << std::endl;
+    //     }
+    //     std::cout << std::endl;
+    // }
+    // MPI_Barrier(comm);
+
+    for(int i = 0; i < countersize-1; i++)
+    {
+        if(globalcounter[i] != 0)
+        {
+            continue;
+        }
+        else
+        {
+            if(globalcounter[i + 1] == 0)
+            {
+                for(int j = i; j < countersize; j++)
+                {
+                    if(globalcounter[j] != 0)
+                    {
+                        globalcounter[i + 1] = globalcounter[j];
+                        globalcounter[j] = 0;
+
+                        keyvalues[i + 1] = keyvalues[j];
+                        keyvalues[j] = SHRT_MIN;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
     unsigned long long int totalsize = 0;
     totalsize += globalcounter[rank];
@@ -46,11 +92,11 @@ void isort(std::vector<short int>& Xi, MPI_Comm comm) {
 
     Xi.resize(totalsize);
 
-    fill_n(Xi.begin(), globalcounter[rank], (2*rank) - size + 1);
+    fill_n(Xi.begin(), globalcounter[rank], keyvalues[rank]);
 
     if((rank + 1) <= (size - 1)) // Handles odd number of nodes case
     {
-        fill_n(Xi.begin() + globalcounter[rank], globalcounter[rank + 1], (2*rank) - size + 2);
+        fill_n(Xi.begin() + globalcounter[rank], globalcounter[rank + 1], keyvalues[rank + 1]);
     }
 
     MPI_Barrier(comm);
@@ -67,7 +113,7 @@ void isort(std::vector<short int>& Xi, MPI_Comm comm) {
             std::cout << "Counts:" << std::endl;
             for(int i = 0; i < countersize; i++)
             {
-                std::cout << i-size+1 << ":\t" << globalcounter[i] << std::endl;
+                std::cout << keyvalues[i] << ":\t" << globalcounter[i] << std::endl;
             }
             std::cout << std::endl;
         }
@@ -83,7 +129,7 @@ void isort(std::vector<short int>& Xi, MPI_Comm comm) {
         //     std::cout << "Counts:" << std::endl;
         //     for(int i = 0; i < countersize; i++)
         //     {
-        //         std::cout << i-size+1 << ":\t" << globalcounter[i] << std::endl;
+        //         std::cout << keyvalues[i] << ":\t" << globalcounter[i] << std::endl;
         //     }
         //     std::cout << std::endl;
         // }
