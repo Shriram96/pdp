2df2085d6ad68339bb3ae0de566e1d9dca3dee34
Shriram Ravi <shriramr@srv-p22-13.cbls.ccr.buffalo.edu>
2021-11-21 23:26:38 -0500

    Tested for small size

--- Project-1/A2/a2.py
+++ Project-1/A2/a2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from pyspark import SparkConf, SparkContext
 import sys
+import time
 
 def edge(line):
     l = line.split(" ")
@@ -11,6 +12,9 @@ def edge_reverse(line):
     l = line.split(" ")
     e = [int(x) for x in l]
     return [(e[0], e[1])]
+
+def edge_count(record):
+    return [(record[1], 1)]
 
 def reverser(record):
     return record[1], record[0]
@@ -24,13 +28,12 @@ def sample(record):
         rand.append((i, record[0]))
     return rand
 
-def processRecord(record):
-    print("Dale", record)
 
 if __name__ == "__main__":
     infile = sys.argv[1]
     outdir = sys.argv[2]
     
+    start = time.time()
     conf = SparkConf().setAppName("PointerJumper")
     sc = SparkContext(conf = conf)
 
@@ -38,30 +41,52 @@ if __name__ == "__main__":
 
     edge_map = lines.flatMap(edge)
     edge_map.persist()
-    datacollector = edge_map.collect()
-    for row in datacollector:
-        print("Edges Dale", row[0], ":", row[1])
+    # datacollector = edge_map.collect()
+    # for row in datacollector:
+    #     print("Edges Dale", row[0], ":", row[1])
 
-    count = edge_map.reduceByKey(lambda a, b: [a] + [b]).count()
-    print("Initial Count Dale: ", count)
+    count = edge_map.flatMap(edge_count).reduceByKey(lambda a, b: a + b).count()
+    # print("Initial Count Dale: ", count)
 
-    E1 = lines.flatMap(edge_reverse)
-    datacollector = E1.collect()
-    for row in datacollector:
-        print("Reverse Edges Dale", row[0], ":", row[1])
+    E1 = edge_map.map(reverser)
+    E1.persist()
+    # datacollector = E1.collect()
+    # for row in datacollector:
+    #     print("Reverse Edges Dale", row[0], ":", row[1])
 
-    looper = 5
-    while(looper):
+    looper = 0
+    while(True):
+        looper += 1
+
         E1 = edge_map.join(E1).map(joinmap)
-        datacollector = E1.collect()
-        for row in datacollector:
-            print("Join 1 Dale", row[0], ":", row[1])
-        distinct_items = E1.countByValue().items()
-        print("Each Count Dale", distinct_items, " ", len(distinct_items))
-
-        looper -= 1
+        E1.persist()
+        edge_map = E1.map(reverser)
+        edge_map.persist()
+        # datacollector = E1.collect()
+        # for row in datacollector:
+        #     print("Join step Dale", row[0], ":", row[1])
+        # datacollector = edge_map.collect()
+        # for row in datacollector:
+        #     print("Edge list step Dale", row[0], ":", row[1])
+        # distinct_items = E1.countByValue().items()
+        # print("Each Count Dale", distinct_items, " ", len(distinct_items))
+        distinct_root = E1.flatMap(edge_count).reduceByKey(lambda a, b: a + b)
+        # datacollector = distinct_root.collect()
+        # for row in datacollector:
+        #     print("Join 1 Dale", row[0], ":", row[1])
+        distinct_root_count = distinct_root.count()
+        # print("Each distinct Count Dale: ", distinct_root_count)
+        
+        if count == distinct_root_count:
+            print("Dale Connected Components Count: ", distinct_root_count)
+            print("Dale Looper total runs: ", looper)
+            break
+        else:
+            count = distinct_root_count
         
 
     E1.saveAsTextFile(outdir)
-    print("Dale", infile, outdir)
+    # print("Dale", infile, outdir)
     sc.stop()
+    end = time.time()
+    print("Dale", end - start)
