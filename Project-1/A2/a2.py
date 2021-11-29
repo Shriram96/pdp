48aab7c73c3c34d1d7d15e03a7fa7a414c9d58d2
Shriram Ravi <shriramr@srv-p22-12.cbls.ccr.buffalo.edu>
2021-11-29 15:52:21 -0500

    Final A2

--- Project-1/A2/a2.py
+++ Project-1/A2/a2.py
@@ -8,37 +8,6 @@ def edge(line):
     e = [int(x) for x in l]
     return [(e[1], e[0]), (e[0], str(e[1]))]
 
-def edge_reverse(line):
-    l = line.split(" ")
-    e = [int(x) for x in l]
-    return [(e[0], e[1])]
-
-def edge_count(record):
-    return [(record[1], 1)]
-
-def reverser(record):
-    return record[1], record[0]
-
-def joinmap(record):
-    return record[1][0], record[1][1]
-
-def sample(record):
-    rand = []
-    for i in record[1]:
-        rand.append((i, record[0]))
-    return rand
-
-def record_printer(map, info):
-    datacollector = map.collect()
-    print("##################################################")
-    for row in datacollector:
-        print(info, "Dale", row[0], ":", row[1])
-    print("##################################################")
-
-def rootnodes(record):
-    if record[0] == record[1]:
-        return record[0], record[1]
-
 def unveil(record):
     answer = 0
     return_records = []
@@ -70,6 +39,7 @@ if __name__ == "__main__":
     lines = sc.textFile(infile)
 
     M = lines.flatMap(edge)
+    M.persist()
     R = None
 
     count = 0
@@ -78,16 +48,13 @@ if __name__ == "__main__":
     while(True):
         looper += 1
         R = M.groupByKey().mapValues(list).filter(lambda a: len(a[1]) > 1)
-        # record_printer(R, "Reduce"+str(looper))
         step_count = R.count()
-        # print("Step: ", looper, "RDD count: ", step_count)
         if count == step_count:
             break
         else:
             count = step_count
         M = R.flatMap(unveil).filter(lambda a: a is not None)
-        # record_printer(M2, "Map"+str(looper))
-    # record_printer(R.flatMap(finalmap).filter(lambda a: a is not None), "Last Reduced")
+        M.persist()
     print("Looper count: ", looper)
 
     R.flatMap(finalmap).filter(lambda a: a is not None).saveAsTextFile(outdir)
